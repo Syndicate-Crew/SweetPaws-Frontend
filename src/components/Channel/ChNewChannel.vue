ffe8f64396a141fb46488c7ea77d17af302b3223
Yasiru Wickramasinghe <it19038292@my.sliit.lk>
2021-08-21 15:36:56 +0530

    channel -> create Channelling slot form

--- src/components/Channel/ChNewChannel.vue
+++ src/components/Channel/ChNewChannel.vue
@@ -1,13 +1,195 @@
 <template>
   <div>
-      <h1>this is Insert Channel Form</h1>
-  </div>
+    <div class="container">
+        <div class="row">       
+            <ChHeader  title="Add Channelling Slot" />
+            <form>
+             <div class="row">
+               <div class="row ">
+                  <label  class="form-label text-start fs-5">
+                    <b>Veterinarian Details</b>
+                  </label>
+                  <hr/>
+              </div>
+                <div class="col-md-4 p-2">
+                  <label  class="form-label text-start" required>
+                    <b>First Name :</b>
+                  </label>
+                </div>
+                <div class="col-md-7 p-2">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="example"
+                    placeholder="Enter First Name"
+                    required
+                  />
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-4 p-2">
+                  <label  class="form-label text-start">
+                    <b>Last Name :</b>
+                  </label>
+                </div>
+                <div class="col-md-7 p-2">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="example"
+                    placeholder="Enter Last Name"
+                    required
+                  />
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-4 p-2">
+                  <label  class="form-label text-start">
+                    <b>Email :</b>
+                  </label>
+                </div>
+                <div class="col-md-7 p-2">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="example"
+                    placeholder="Enter Email"
+                    required
+                  />
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-4 p-2">
+                  <label  class="form-label text-start">
+                    <b>Phone :</b>
+                  </label>
+                </div>
+                <div class="col-md-7 p-2">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="example"
+                    placeholder="Enter Phone"
+                    required
+                  />
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-4 p-2">
+                  <label  class="form-label text-start">
+                    <b>Charge :</b>
+                  </label>
+                </div>
+                <div class="col-md-7 p-2">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="example"
+                    placeholder="Enter veterinarian Charges"
+                    required
+                  />
+                </div>
+              </div>
+
+              <div class="row">
+                  <label  class="form-label text-start fs-5">
+                    <b>Hospital Details</b>
+                  </label>
+                  <hr/>
+              </div>
+
+              <div class="row">
+                <div class="col-md-4 p-2">
+                  <label  class="form-label text-start">
+                    <b>Room No :</b>
+                  </label>
+                </div>
+                <div class="col-md-7 p-2">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="example"
+                    placeholder="Enter Room Number"
+                    required
+                  />
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-4 p-2">
+                  <label  class="form-label text-start">
+                    <b>Date :</b>
+                  </label>
+                </div>
+                <div class="col-md-7 p-2">
+                  <input
+                    type="date"
+                    class="form-control"
+                    id="example"
+                    placeholder="Enter Date"
+                    required
+                  />
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-4 p-2">
+                  <label  class="form-label text-start">
+                    <b>Time :</b>
+                  </label>
+                </div>
+                <div class="col-md-7 p-2">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="example"
+                    placeholder="Enter Time"
+                    required
+                  />
+                </div>
+              </div>
+             <div class="row">
+                <div class="col-md-4 p-2">
+                  <label  class="form-label text-start">
+                    <b>Charge :</b>
+                  </label>
+                </div>
+                <div class="col-md-7 p-2">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="example"
+                    placeholder="Enter Hospital Charge"
+                    required
+                  />
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-md-3 p-2"></div>
+                <div class="col-md-6 p-2">
+                  <button type="button" class="btn btn-success p-2 m-2">
+                    Submit
+                  </button>
+                  <button type="button" class="btn btn-danger p-2 m-2">
+                    Cancel
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
 </template>
 
 <script>
+import ChHeader from "../../components/Channel/Ch_Header.vue"
+
 export default {
 
-name: "ChSlotForm"
+  name: "ChSlotForm",
+    components: {
+      ChHeader,
+    },
+
 }
 </script>
 
--- src/views/Channel/ChNewChannel.vue
+++ src/views/Channel/ChNewChannel.vue
@@ -2,7 +2,16 @@
   <div>
       <Navbar/>
       <ChHeader title="New Channel Slot"/>
-      <ChSlotForm/>
+        <div>
+        <div class="row">
+          <div class="col-md-2 border border-primary form-card">
+            
+          </div>
+          <div class="col-md-7 container center card rounded m-5 p-2 shadow-lg border border-primary form-card">
+            <ChSlotForm/>
+          </div>
+        </div>
+      </div>   
   </div>
 </template>
 
