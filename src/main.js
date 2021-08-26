f68c71f0d8435b8b3e0471e1d61c0341bfa1e653
Yasiru Wickramasinghe <it19038292@my.sliit.lk>
2021-08-26 16:41:47 +0530

    enable alert

--- src/main.js
+++ src/main.js
@@ -2,11 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import { BootstrapVue } from 'bootstrap-vue'
-<<<<<<< HEAD
 import VueSweetalert2 from 'vue-sweetalert2';
-=======
 import Vuelidate from 'vuelidate';
->>>>>>> d18bb42ea29a84cb3f00041adb1efb289a5066f2
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -14,11 +11,8 @@ import 'sweetalert2/dist/sweetalert2.min.css';
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
-<<<<<<< HEAD
 Vue.use(VueSweetalert2)
-=======
 Vue.use(Vuelidate);
->>>>>>> d18bb42ea29a84cb3f00041adb1efb289a5066f2
 
 new Vue({
   router,
