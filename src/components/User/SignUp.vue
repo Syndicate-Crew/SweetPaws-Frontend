d18bb42ea29a84cb3f00041adb1efb289a5066f2
MalinduPanchala <malindupanchala@gmail.com>
2021-08-26 16:34:13 +0530

    Minor changes and fixes

--- src/components/User/SignUp.vue
+++ src/components/User/SignUp.vue
@@ -2,34 +2,67 @@
     <b-container fluid class="w-75 form-wrapper">
         <b-col class="form-component-wrapper">
             <div class="form-title">Sign up</div>
-            <b-row class="form-col-row" cols-sm="1" cols="2" cols-md="2">
-                <b-col>
-                    <div class="form-image-container">
-                        <div class="form-image-div overlay-background"></div>
-                        <div class="form-image-div overlay">
-                            <label for="file-upload" class="custom-file-upload">
-                                <!-- click here to select file -->
-                                <i class="bi bi-upload"></i>
-                            </label>
-                            <input id="file-upload" type="file"/>
+            <form @submit.prevent="handleSubmit">
+                <b-row class="form-col-row" cols-sm="1" cols="2" cols-md="2">
+                    <b-col>
+                        <div class="form-image-container">
+                            <img class="form-image-div overlay-background"  src="../../assets/person.png" id="image-id" alt="">
+                            <div class="form-image-div overlay">
+                                <label for="file-upload" class="custom-file-upload">
+                                    <!-- click here to select file -->
+                                    <i class="bi bi-upload"></i>
+                                </label>
+                                <input id="file-upload" accept="image/png, image/gif, image/jpeg" @change="previewImage" multiple="false" type="file"/>
+                            </div>
                         </div>
-                    </div>
-                </b-col>
-                <b-col cols="8">
-                    <b-input id="name" type="text" class="w=75 form-input" autocomplete="false" placeholder="Name"></b-input>
-                    <b-input id="email" type="email" class="w=75 form-input" autocomplete="false" placeholder="E Mail"></b-input>
-                    <b-input id="password" type="password" class="w=75 form-input" autocomplete="false" placeholder="Password"></b-input>
-                    <div class="show-password"><span><input type="checkbox"/></span>Show Password</div>
-                    <b-input id="confirm-password" type="password" class="w=75 form-input" autocomplete="false" placeholder="Confirm password"></b-input>
-                </b-col>
-            </b-row>
-            <b-row class="form-actions">
-                <button class="form-btn" @click="$event.preventDefault()">Reset</button>
-                <button class="form-btn" @click="$event.preventDefault()" style="background: #5F30E2;color: white">Submit</button>
-            </b-row>
+                    </b-col>
+                    <b-col cols="8">
+                        <div class="form-group">
+                            <input type="text" v-model="user.name" id="name" name="name" class="form-control w=75 form-input" placeholder="Name" autocomplete="false" :class="{ 'is-invalid': submitted && $v.user.name.$error }" />
+                            <div v-if="submitted && !$v.user.name.required" class="invalid-feedback">First Name is required</div>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" v-model="user.email" id="email" name="email" class="form-control w=75 form-input" placeholder="E mail" autocomplete="false" :class="{ 'is-invalid': submitted && $v.user.email.$error }" />
+                            <div v-if="submitted && $v.user.email.$error" class="invalid-feedback">
+                                <span v-if="!$v.user.email.required">Email is required</span>
+                                <span v-if="!$v.user.email.email">Email is invalid</span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <input :type="passwordType" v-model="user.password" id="password" name="password" autocomplete="false" placeholder="Password" class="form-control w=75 form-input" :class="{ 'is-invalid': submitted && $v.user.password.$error }" />
+                            <div v-if="submitted && $v.user.password.$error" class="invalid-feedback">
+                                <span v-if="!$v.user.password.required">Password is required</span>
+                                <span v-if="!$v.user.password.minLength">Password must be at least 6 characters</span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <input type="checkbox" @change="togglePasswordType" id="showPassword"/>
+                            <label for="showPassword" style="margin-left: 5px">Show Password</label>
+                        </div>
+                        <div class="form-group">
+                            <input type="password" v-model="user.confirmPassword" id="confirmPassword" name="confirmPassword" placeholder="Confirm Password" autocomplete="false"  class="form-control w=75 form-input" :class="{ 'is-invalid': submitted && $v.user.confirmPassword.$error }" />
+                            <div v-if="submitted && $v.user.confirmPassword.$error" class="invalid-feedback">
+                                <span v-if="!$v.user.confirmPassword.required">Confirm Password is required</span>
+                                <span v-else-if="!$v.user.confirmPassword.sameAsPassword">Passwords must match</span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <input type="number" v-model="user.phone" id="phone" name="confirmPassword" placeholder="Phone" autocomplete="false"  class="form-control w=75 form-input" :class="{ 'is-invalid': submitted && $v.user.phone.$error }" />
+                            <div v-if="submitted && $v.user.phone.$error" class="invalid-feedback">
+                                <span v-if="!$v.user.phone.required">Phone number is required</span>
+                                <span v-else-if="!$v.user.phone.minLength">Phone number must have 10 digits</span>
+                            </div>
+                        </div>
+                    </b-col>
+                </b-row>
+                <b-row class="form-actions">
+                    <button class="form-btn" @click.prevent="reset">Reset</button>
+                    <button class="form-btn" style="background: #5F30E2;color: white">Submit</button>
+                </b-row>
+            </form>
         </b-col>
         <div class="form-hint">
-                    <center>Already have an account? <span class="form-hint-link">Sign in</span></center>
+                    <center>Already have an account? <span class="form-hint-link" @click="window.location=''">Sign in</span></center>
                 </div>
     </b-container>
 </template>
@@ -61,7 +94,7 @@
 }
 
 .form-input {
-    margin: 13px 0px 19px 0px;
+    margin: 19px 0px 3px 0px;
     border: 2px solid #7E7DDE;
 }
 
@@ -107,16 +140,23 @@
     overflow: hidden;
     position: relative;
     border-radius: 19px;
+    border: 1px solid #171859;
 }
 
 .form-image-div {
-    background-color: #171859;
     width: inherit;
     height: inherit;
 }
 
 .overlay-background {
-    background-color: yellow;
+    background-color: white;
+}
+
+.image {
+    width: inherit;
+    height: auto;
+    margin: 0;
+    padding: 0;
 }
 
 .overlay {
@@ -145,6 +185,13 @@
     transform: translate(-50%,-50%);
     left: 50%;
     top: 50%;
+    padding: 20px 25px;
+    border-radius: 50%;
+    background-color: #7E7DDE;
+}
+
+.overlay-background {
+    background-color: white;
 }
 
 .custom-file-upload:hover {
@@ -152,3 +199,76 @@
 }
 
 </style>
+
+<script>
+import { required, email, minLength, sameAs } from "vuelidate/lib/validators"
+
+export default {
+    name: "SignUp",
+    created() {
+        this.image = require("../../assets/person.png")
+    },
+    data() {
+        return {
+            user: {
+                name: "",
+                email: "",
+                password: "",
+                confirmPassword: "",
+                phone: ""
+            },
+            submitted: false,
+            passwordType: "password",
+            image: require("../../assets/person.png"),
+        };
+    },
+    validations: {
+        user: {
+            name: { required },
+            email: { required, email },
+            password: { required, minLength: minLength(6) },
+            confirmPassword: { required, sameAsPassword: sameAs('password') },
+            phone: { required, minLength: minLength(10) }
+        }
+    },
+    methods: {
+        handleSubmit() {
+            this.submitted = true;
+
+            // stop here if form is invalid
+            this.$v.$touch();
+            if (this.$v.$invalid) {
+                return;
+            }
+
+            alert("SUCCESS!! :-)\n\n" + JSON.stringify(this.user));
+        },
+        togglePasswordType() {
+            if (this.passwordType == "password") {
+                this.passwordType = "text"
+            } else {
+                this.passwordType = "password"
+            }
+        },
+        previewImage(e) {
+            const file = e.target.files[0];
+            document.getElementById("image-id").src = URL.createObjectURL(file);
+            this.image = file;
+        },
+        reset() {
+            console.log(this.image);
+            this.user = {
+                name: "",
+                email: "",
+                password: "",
+                confirmPassword: "",
+                phone: ""
+            }
+            this.submitted = false
+            this.image = "../../assets/person.png"
+            document.getElementById("image-id").src = require("../../assets/person.png");
+
+        }
+    }
+};
+</script>
--- src/views/User/SignInView.vue
+++ src/views/User/SignInView.vue
@@ -1,18 +1,17 @@
 <template>
     <div>
-        <Header />
+        <!-- <Header /> -->
         <SignIn />
     </div>
 </template>
 
 <script>
 import SignIn from "../../components/User/SignIn.vue";
-import Header from "../../components/Header/Header.vue"
+// import Header from "../../components/Header/Header.vue"
 export default {
     name: "SignInVIew",
     components: {
         SignIn,
-        Header
     }
 }
 </script>
