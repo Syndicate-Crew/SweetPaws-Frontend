46eed1ee0c6672205c997e332d5b4d654a330e06
MalinduPanchala <malindupanchala@gmail.com>
2021-09-19 13:38:34 +0530

    Changes in ListItem and UserListContainer

--- src/components/User/Admin/UserManagement/UserList/UserListContainer.vue
+++ src/components/User/Admin/UserManagement/UserList/UserListContainer.vue
@@ -2,9 +2,9 @@
 	<div class="user-list-container">
 		<div class="options-div">
 			<label for="user-list-sort">Sort by  </label>
-			<b-form-select id="user-list-sort" class="user-list-sort-select">
-				<b-form-select-option>A - Z</b-form-select-option>
-				<b-form-select-option>Z - A</b-form-select-option>
+			<b-form-select v-model="sort" id="user-list-sort" class="user-list-sort-select" @change="onChangeHandler">
+				<b-form-select-option value="1">A - Z</b-form-select-option>
+				<b-form-select-option value="2">Z - A</b-form-select-option>
 			</b-form-select>
 		</div>
 		<div v-for="user in users" :key="user">
@@ -19,7 +19,7 @@
 }
 
 .user-list-sort-select {
-	margin: 0px 20px;
+	margin: 0px 20px 20px 20px;
 }
 </style>
 
@@ -29,11 +29,22 @@ export default {
 	name: "UserListContainer",
 	data: function() {
 		return {
-			users: ["User1", "User2"]
+			users: ["User1", "User2"],
+			sort: 1,
 		}
 	},
 	components: {
 		ListItem,
 	},
+	methods: {
+		onChangeHandler() {
+			if (this.sort == 1) {
+				alert(1);
+			} else if ( this.sort == 2) {
+				alert(2)
+			}
+
+		}
+	}
 };
 </script>
