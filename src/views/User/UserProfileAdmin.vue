c2274c29be5632431ea7c4e6bd5e62e13f62e6d6
MalinduPanchala <malindupanchala@gmail.com>
2021-09-20 18:10:59 +0530

    Profile for Admin initialized

---
+++ src/views/User/UserProfileAdmin.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <UserProfile />
+    </div>
+</template>
+
+<script>
+import UserProfile from "../../components/User/Admin/UserManagement/UserProfile.vue";
+export default {
+    name: "User",
+    components: {
+        UserProfile
+    }
+}
+</script>
