b0f8c4cc235aa3ecf3a280756ce7b121349aab5f
MalinduPanchala <malindupanchala@gmail.com>
2021-10-01 19:47:21 +0530

    UserProfileAdmin updated
    Header and Footer added

--- src/views/User/UserProfileAdmin.vue
+++ src/views/User/UserProfileAdmin.vue
@@ -1,15 +1,21 @@
 <template>
     <div>
+        <Header />
         <UserProfile />
+        <Footer />
     </div>
 </template>
 
 <script>
 import UserProfile from "../../components/User/Admin/UserManagement/UserProfile.vue";
+import Header from "../../components/Header/Header.Component.vue"
+import Footer from "../../components/Footer/Footer.Component.vue"
 export default {
     name: "User",
     components: {
-        UserProfile
+        UserProfile,
+        Header,
+        Footer
     }
 }
 </script>
