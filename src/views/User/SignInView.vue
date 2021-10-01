5049fc4296a0e8a9a25042b84b167173c57b2033
MalinduPanchala <malindupanchala@gmail.com>
2021-10-01 19:45:28 +0530

    SignInView for users updated
    Footer added

--- src/views/User/SignInView.vue
+++ src/views/User/SignInView.vue
@@ -2,17 +2,20 @@
     <div>
         <Header />
         <SignIn />
+        <Footer />
     </div>
 </template>
 
 <script>
 import SignIn from "../../components/User/SignIn.vue";
 import Header from "../../components/Header/Header.Component.vue"
+import Footer from "../../components/Footer/Footer.Component.vue"
 export default {
     name: "SignInVIew",
     components: {
         SignIn,
-        Header
+        Header,
+        Footer
     }
 }
 </script>
