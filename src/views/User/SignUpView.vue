708d0301d5796ea65fb7a9cac6d6066d8406aefc
MalinduPanchala <malindupanchala@gmail.com>
2021-10-01 19:45:49 +0530

    SignUpView updated
    Footer added

--- src/views/User/SignUpView.vue
+++ src/views/User/SignUpView.vue
@@ -2,17 +2,20 @@
     <div>
         <Header />
         <SignUp />
+        <Footer />
     </div>
 </template>
 
 <script>
 import SignUp from "../../components/User/SignUp.vue";
 import Header from "../../components/Header/Header.Component.vue"
+import Footer from "../../components/Footer/Footer.Component.vue"
 export default {
     name: "SignUpView",
     components: {
         SignUp,
-        Header
+        Header,
+        Footer
     }
 }
 </script>
