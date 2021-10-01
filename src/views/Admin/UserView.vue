0796f714b722de73bbe0dccc0e19017fcd73edaa
MalinduPanchala <malindupanchala@gmail.com>
2021-10-01 19:45:03 +0530

    UserView updated
    Footer added

--- src/views/Admin/UserView.vue
+++ src/views/Admin/UserView.vue
@@ -2,17 +2,20 @@
     <div>
         <Header />
         <UserProfile />
+        <Footer />
     </div>
 </template>
 
 <script>
 import UserProfile from "../../components/User/Admin/UserManagement/UserProfile.vue";
 import Header from "../../components/Header/Header.Component.vue"
+import Footer from "../../components/Footer/Footer.Component.vue"
 export default {
     name: "SignInVIew",
     components: {
         Header,
-        UserProfile
+        UserProfile,
+        Footer
     }
 }
 </script>
