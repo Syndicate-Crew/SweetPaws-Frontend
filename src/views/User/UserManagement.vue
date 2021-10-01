293c4dc6a72e6a732e71b3e0a794a0fa9281d90e
MalinduPanchala <malindupanchala@gmail.com>
2021-10-01 19:46:57 +0530

    UserManagementView updated
    Header and footer added

--- src/views/User/UserManagement.vue
+++ src/views/User/UserManagement.vue
@@ -1,15 +1,21 @@
 <template>
     <div>
+        <Header />
         <UserManagement />
+        <Footer />
     </div>
 </template>
 
 <script>
 import UserManagement from "../../components/User/Admin/UserManagement/UserManagement.vue";
+import Header from "../../components/Header/Header.Component.vue"
+import Footer from "../../components/Footer/Footer.Component.vue"
 export default {
     name: "User",
     components: {
-        UserManagement
+        UserManagement,
+        Header,
+        Footer
     }
 }
 </script>
